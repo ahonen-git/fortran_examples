6c420d25af3525c08f5ad4aac5b6828a9c4f46e1
ahonen-git <misumi@criepi.denken.or.jp>
2021-08-08 16:11:57 +0900

    継承の追加

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ fortranのサンプルファイル
 
 ## 参考
 https://qiita.com/cometscome_phys/items/f87080286c6fdf72e49a
+
+## ブランチの作業
+- 210808_01
+	- 継承の追加。
--- test.f90
+++ test.f90
@@ -1,4 +1,3 @@
-
 !---------------------------------------------------
 module sample
 implicit none
@@ -8,33 +7,48 @@ implicit none
 type, public :: people
    character(len=60), private :: f_name = "noname"
    character(len=60), private :: l_name = "noname"
-   character(len=3) , private :: keisyou
+   character(len=3) , private :: keisyou = "noname"
 contains
    procedure :: set_name => people_set_name
    procedure :: display => people_display
 end type people
 
-! コンストラクタ：インスタンス生成時に実行されるメソッドのこと。
+! 継承はextendで行う。
+type, extends(people) :: boy
+contains
+   procedure :: say_hi => boy_say_hi
+end type boy
+
+
+! コンストラクタ
+! インスタンス生成時に実行されるメソッドのこと。
 ! initをpeopleでオーバーロードすることで、
 ! people()としたときにinitを実行させてインスタンスを生成する。
 ! peopleというクラス名と一致させなくても動作する。
 ! fortranではコンストラクタはなくても良い。
-! オブジェクトを模擬しているだけなのか？
 interface people
    module procedure init
 end interface people
 
+interface boy
+   module procedure init_boy
+end interface boy
+
 contains
 
-! result文は関数の戻り値を関数名の変数にしないための命令。
+! resultは関数の戻り値を関数名の変数にしないための命令。
 ! ここでは生成しているクラスを返す。
 ! peopleの型で定義する。
-! functionの定義文では「::」を使わないので、「::」は入れない。
 type(people) function init(kei) result(self)
    character(len=3), intent(in) :: kei
    self%keisyou=kei
    write(6,*) "people instance is generated."
 end function init
+
+type(boy) function init_boy() result(self)
+   write(6,*) "boy instance is generated."
+end function init_boy
+
 
 ! メソッドとなるサブルーチンでクラスの変数を使用するために
 ! peopleの型でselfを作る。
@@ -51,6 +65,11 @@ subroutine people_display(self)
    write(*,"(A10,X,A10,X,A3)") self%f_name, self%l_name, self%keisyou
 end subroutine people_display
 
+subroutine boy_say_hi(self)
+   class(boy), intent(in) :: self
+   write(*,*) "Hi, I am a boy."
+end subroutine boy_say_hi
+
 end module sample
 !---------------------------------------------------
 program test
@@ -58,10 +77,14 @@ program test
    implicit none
 
    type(people) :: a
+   type(boy) :: b
+
    a=people("san")
    call a%set_name("Taro","Tanaka")
    call a%display()
 
+   b=boy()
+   call b%say_hi()
 
 end program test
 !---------------------------------------------------
